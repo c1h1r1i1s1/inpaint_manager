632946298a552ba1731252b537d1ab8c06b2dfa5
c1h1r1i1s1 <christianfaney@gmail.com>
2025-05-08 22:15:55 +1000

    Working before dual-eye testing

--- inpaint_manager.cpp
+++ inpaint_manager.cpp
@@ -16,7 +16,7 @@ const wchar_t* EVENT_INPUT_READY = L"InputReadyEvent";
 const wchar_t* EVENT_OUTPUT_READY = L"OutputReadyEvent";
 
 int main() {
-    inpaintingModel = new InpaintingModel("st_360_reshape_stage1_epoch15_win.engine");
+    inpaintingModel = new InpaintingModel("st_360_reshape_stage2_epoch2.engine");
     // Create or open the shared memory region.
     HANDLE hMapFile = CreateFileMappingW(
         INVALID_HANDLE_VALUE,    // Use the system paging file.
